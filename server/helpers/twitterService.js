605ff240ee349bb3a8108f2f28edd0df6dd45905
arul21 <arul.21@hotmail.com>
2021-01-10 16:38:57 +0800

    env

--- server/helpers/twitterService.js
+++ server/helpers/twitterService.js
@@ -1,13 +1,9 @@
 const Twitter = require("twitter");
 
 const client = new Twitter({
-  consumer_key: "11HqP6GeMu08eTHjDRwqroOF1",
-  consumer_secret: "4ONLczOvWH8YSpfOvwdrZhBAThlSnMqCuVmGAvihQv08gWHoZF",
-  bearer_token:
-    "AAAAAAAAAAAAAAAAAAAAAFogLgEAAAAA72IXEJlZwFa2YYiZZ5a6tdMqFus%3D9JMfCsntCD2FZayB1BieUBJL2YYH1poFhjbVjRie55dWNrO5OZ",
-
-  // access_token_key: "",
-  // access_token_secret: "",
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  bearer_token: process.env.BEARER_TOKEN,
 });
 
 module.exports = client;
