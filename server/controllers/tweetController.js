71d5d48a3a7b9bf0964875bb405fa6c9dab7039b
arul21 <arul.21@hotmail.com>
2021-01-18 15:33:42 +0800

    fix-get-tweet

--- server/controllers/tweetController.js
+++ server/controllers/tweetController.js
@@ -1,20 +1,64 @@
-const e = require("express");
+const Tweet = require("../models/tweet");
 const client = require("../helpers/twitterService");
+const { get } = require("lodash");
 
 module.exports = {
   getTweet: (req, res) => {
-    let loc = "-6.2000,106.816666,400km";
+    let loc = "-5.9179251,118.9500887,12000km";
     client.get(
       "search/tweets",
-      { q: "#covid", count: 2 },
+      { q: "covid-19", geocode: loc, count: 1, result_type: "recent" },
       function (error, tweets, response) {
         if (!error) {
-          console.log(tweets.statuses.length);
-          res.status(200).json(tweets);
+          const data = tweets.statuses[0];
+
+          res.status(200).json({
+            test: get(data, "metadata.iso_language_code", "en"),
+            tweets,
+          });
         } else {
           res.status(500).json(error);
         }
       }
     );
+  },
+
+  getNewTweet: async () => {
+    let loc = "-5.9179251,118.9500887,12000km";
+    client.get(
+      "search/tweets",
+      { q: "covid-19", geocode: loc, count: 1, result_type: "recent" },
+      async (error, tweets, response) => {
+        if (!error) {
+          const data = tweets.statuses[0];
+          const findTweet = await Tweet.find({ id: data.id });
+          const isFound =
+            findTweet.length === 0 &&
+            get(data, "metadata.iso_language_code", "en") === "in";
+          console.log(isFound, get(data, "metadata.iso_language_code", "en"));
+          if (isFound) {
+            const response = await Tweet.create({
+              id: data.id,
+              text: data.text,
+              user: data.user,
+              created_at: data.created_at,
+            });
+            console.log("Successfully save to db");
+          }
+        } else {
+          console.log(error);
+          // res.status(500).json(error);
+        }
+      }
+    );
+  },
+
+  getAllTweet: async (req, res) => {
+    try {
+      const response = await Tweet.find({ deleteAt: null });
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   },
 };
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { getTweet } = require("../controllers/tweetController");
+const {
+  getTweet,
+  getNewTweet,
+  getAllTweet,
+} = require("../controllers/tweetController");
+const cron = require("node-cron");
 
 /* GET users listing. */
+
+cron.schedule("*/10 * * * * *", function () {
+  console.log("running a task every 10 second");
+  getNewTweet();
+});
+
 router.get("/", getTweet);
+router.get("/all", getAllTweet);
 
 module.exports = router;
